64348c439e80cf22ad9b9562531ee42d7571101d
Michael <mjhaddis@gmail.com>
2025-04-01 12:54:50 +0200

    login done

--- front-end/src/login.ts
+++ front-end/src/login.ts
@@ -1,7 +1,35 @@
+import axios from "axios";
 import "./style.css";
 
-document.getElementById("loginForm")?.addEventListener("submit", (e) => {
+document.getElementById("loginForm")?.addEventListener("submit", async (e) => {
     e.preventDefault()
 
     const userEmail = (document.getElementById("email") as HTMLInputElement).value
+
+    const userPassword = (document.getElementById("password") as HTMLInputElement).value
+
+    if (!userEmail || !userPassword) {
+        alert("Please enter both email and password.")
+        return
+    }
+
+    try {
+        const response = await axios.post(
+            "http://localhost:3000/login",
+            {
+                email: userEmail,
+                password: userPassword
+            },
+            {
+                withCredentials: true  
+            }
+        )
+        console.log(response.data)
+    
+        location.href = "/"
+      
+    } catch (error) {
+        alert("Login failed. Check email and password")
+        console.error(error)
+    }
 })
