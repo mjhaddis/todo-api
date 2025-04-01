1b1a89a70900106b7e069f59936328c9782d8e8f
Michael <mjhaddis@gmail.com>
2025-04-01 11:33:36 +0200

    added event for form

---
+++ front-end/src/login.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.getElementById("loginForm")?.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const userEmail = (document.getElementById("email") as HTMLInputElement).value
+})
